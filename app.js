e259745b9cf2869ddb872dcf72faa598d00e932e
tolu-staarm <87914902+tolu-staarm@users.noreply.github.com>
2022-03-10 15:06:33 +0100

    Middleware

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -9,6 +9,10 @@ app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(3000);
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     const blogs = [
